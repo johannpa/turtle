e28902773631c214ea8e8ebeb2dd8183f22a2012
johannpa <johann_paque@hotmail.fr>
2023-05-22 21:16:38 +0200

    The turtle movments

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import turtle
+import turtle # import turtle module
 
-t = turtle.Turtle()
+t = turtle.Turtle() # create a turtle object
 
-t.forward(100)
-t.left(90)
-t.forward(50)
-t.backward(100)
-t.right(45)
-t.forward(200)
+t.forward(100) # move forward 100 pixels
+t.left(90) # turn left 90 degrees
+t.forward(50)   # move forward 50 pixels
+t.backward(100) # move backward 100 pixels
+t.right(45)    # turn right 45 degrees
+t.forward(200) # move forward 200 pixels
 
-turtle.done()
+turtle.done() # keep the window open until it is closed
